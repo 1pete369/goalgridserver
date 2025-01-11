f26677f5909a765a46f7da146d40474404cae10d
REDDY PRO <22135a0508@gvpce.ac.in>
2025-01-11 11:29:57 +0530

    chat basic created

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB "))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // Pusher setup
